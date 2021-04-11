84fba8d97c67e6921b5611d0410a5571c7230c6d
Francisco Javier Arceo <franciscojavierarceo@users.noreply.github.com>
2021-04-11 12:40:42 -0400

    Create pull_all_tweets.py

---
+++ demos/pull_all_tweets.py
@@ -0,0 +1,38 @@
+import tweepy
+
+def get_all_tweets(screen_name: str) -> list:
+    #initialize a list to hold all the tweepy Tweets
+    alltweets = []  
+
+    #make initial request for most recent tweets (200 is the maximum allowed count)
+    new_tweets = api.user_timeline(screen_name = screen_name, count=200)
+
+    #save most recent tweets
+    alltweets.extend(new_tweets)
+
+    #save the id of the oldest tweet less one
+    oldest = alltweets[-1].id - 1
+
+    #keep grabbing tweets until there are no tweets left to grab
+    while len(new_tweets) > 0:
+        print(f"getting tweets before {oldest}")
+        #all subsiquent requests use the max_id param to prevent duplicates
+        new_tweets = api.user_timeline(screen_name = screen_name,count=200, max_id=oldest)
+        #save most recent tweets
+        alltweets.extend(new_tweets)
+        #update the id of the oldest tweet less one
+        oldest = alltweets[-1].id - 1
+        print(f"...{len(alltweets)} tweets downloaded so far")
+    return alltweets
+
+def main():
+    TWITTER_API_KEY = 'YOUR_TWITTER_API_KEY'
+    TWITTER_API_SECRET = 'YOUR_TWITTER_API_SECRET'
+    USER_NAME = 'ENTER_USER_NAME_HERE'
+    auth = tweepy.OAuthHandler(TWITTER_API_KEY, TWITTER_API_SECRET)
+    api = tweepy.API(auth)
+    tweets = get_all_tweets(api, USER_NAME)    
+    print(tweets)
+
+if __name__=='__main__':
+    main()
