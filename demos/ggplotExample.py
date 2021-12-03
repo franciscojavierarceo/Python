99d430407ac3460e8c8315e1d6783943eb008e2f
Francisco Javier Arceo <franciscojavierarceo@users.noreply.github.com>
2021-12-03 06:35:25 -0700

    Update ggplotExample.py

--- demos/ggplotExample.py
+++ demos/ggplotExample.py
@@ -1,10 +1,9 @@
-from ggplot import *
+import ggplot as g
+
 def ggplotexample():
-#	print diamonds.head()
-	print meat.head()
-	p = ggplot(aes(x='date',y='beef'),data=meat)
-	print p + geom_point(color='blue')+theme_bw()+stat_smooth(color='red')
+	print(meat.head())
+	p = g.ggplot(aes(x='date',y='beef'),data=meat)
+	print p + g.geom_point(color='blue') + g.theme_bw() + g.stat_smooth(color='red')
 		
 if __name__== "__main__":
-	ggplotexample()
-	
+	ggplotexample()	
