76d13fbe954349c169c618b20a52acf78848adee
franciscojavierarceo <arceofrancisco@gmail.com>
2022-04-09 09:52:09 -0600

    used black to format it

--- demos/beam-flink/flink/k8s/beam_flink_job.py
+++ demos/beam-flink/flink/k8s/beam_flink_job.py
@@ -4,21 +4,26 @@ from apache_beam.io import WriteToText
 from apache_beam.options.pipeline_options import PipelineOptions
 import apache_beam.transforms.window as window
 
+
 def run():
-    options = PipelineOptions([
-        "--runner=FlinkRunner",
-        "--flink_version=1.10",
-        "--flink_master=localhost:8081",
-        "--environment_type=EXTERNAL",
-        "--environment_config=localhost:50000"
-    ])
+    options = PipelineOptions(
+        [
+            "--runner=FlinkRunner",
+            "--flink_version=1.10",
+            "--flink_master=localhost:8081",
+            "--environment_type=EXTERNAL",
+            "--environment_config=localhost:50000",
+        ]
+    )
 
     with beam.Pipeline(options=options) as p:
-        (p
-         | 'Create words' >> beam.Create(['to be or not to be'])
-         | 'Split words' >> beam.FlatMap(lambda words: words.split(' '))
-         | 'Write to file' >> WriteToText('test.txt')
-         )
+        (
+            p
+            | "Create words" >> beam.Create(["to be or not to be"])
+            | "Split words" >> beam.FlatMap(lambda words: words.split(" "))
+            | "Write to file" >> WriteToText("test.txt")
+        )
+
 
 if __name__ == "__main__":
     run()
