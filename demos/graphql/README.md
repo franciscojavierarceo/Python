5f888890c51c79fb2b44a5641f938c8091e27267
franciscojavierarceo <arceofrancisco@gmail.com>
2022-04-10 07:38:48 -0600

    updated readme

---
+++ demos/graphql/README.md
@@ -0,0 +1,11 @@
+# Graphql in Python
+
+This is a light demo that shows graphql in a containerized flask application that allows you to interact with the query UI on http://127.0.0.1:5001/graphql by simply executing:
+
+```
+docker build .
+docker-compose up
+```
+In `queries.graphql` you will see several valid queries that you can use to interact with the database / api.
+
+Note, I use port 5001 because I had issue with 5000. 🤷‍♂️
--- demos/graphql/app.py
+++ demos/graphql/app.py
@@ -9,15 +9,23 @@ from ariadne.constants import PLAYGROUND_HTML
 
 from flask import request, jsonify
 from api.queries import resolve_todos, resolve_todo
+from api.mutations import resolve_create_todo, resolve_mark_done, \
+    resolve_delete_todo, resolve_update_due_date
 
 query = ObjectType("Query")
 
 query.set_field("todos", resolve_todos)
 query.set_field("todo", resolve_todo)
 
+mutation = ObjectType("Mutation")
+mutation.set_field("createTodo", resolve_create_todo)
+mutation.set_field("markDone", resolve_mark_done)
+mutation.set_field("deleteTodo", resolve_delete_todo)
+mutation.set_field("updateDueDate", resolve_update_due_date)
+
 type_defs = load_schema_from_path("schema.graphql")
 schema = make_executable_schema(
-    type_defs, query, snake_case_fallback_resolvers
+    type_defs, query, mutation, snake_case_fallback_resolvers
 )
 
 
