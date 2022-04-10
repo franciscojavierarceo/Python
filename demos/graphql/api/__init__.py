0fe70731fde0d99f31f9369f06e4ed599485d443
franciscojavierarceo <arceofrancisco@gmail.com>
2022-04-09 21:54:16 -0600

    uploading graphql

---
+++ demos/graphql/api/__init__.py
@@ -0,0 +1,17 @@
+import os
+from flask import Flask, render_template
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+CORS(app)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{os.getcwd()}/todo.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+@app.route('/')
+def hello():
+    # return 'My First API !!'
+    return render_template('index.html')
+
---
+++ demos/graphql/app.py
@@ -0,0 +1,9 @@
+import os
+from api import app, db
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5001))
+    app.run(debug=True, host='0.0.0.0', port=port)
