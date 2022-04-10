6c83fdc721256655deb1af21f2ea3248cfa8195c
franciscojavierarceo <arceofrancisco@gmail.com>
2022-04-09 19:50:22 -0600

    linter

--- demos/beam-flink/beam/groupby_demo.py
+++ demos/beam-flink/beam/groupby_demo.py
@@ -1,18 +1,18 @@
 import apache_beam as beam
 
 inputs = [
-    ('🐹', '🌽'),
-    ('🐼', '🎋'),
-    ('🐰', '🥕'),
-    ('🐹', '🌰'),
-    ('🐰', '🥒'),
+    ("🐹", "🌽"),
+    ("🐼", "🎋"),
+    ("🐰", "🥕"),
+    ("🐹", "🌰"),
+    ("🐰", "🥒"),
 ]
 
 with beam.Pipeline() as pipeline:
     outputs = (
-            pipeline
-            | 'Create (animal, food) pairs' >> beam.Create(inputs)
-            | 'Group foods by animals' >> beam.GroupByKey()
+        pipeline
+        | "Create (animal, food) pairs" >> beam.Create(inputs)
+        | "Group foods by animals" >> beam.GroupByKey()
     )
 
     outputs | beam.Map(print)
