eff031532fa98e50358a6484994d95d62e969682
franciscojavierarceo <arceofrancisco@gmail.com>
2022-05-21 23:58:27 -0400

    adding something

--- demos/feast/feature_repo/print_features.py
+++ demos/feast/feature_repo/print_features.py
@@ -1,3 +1,5 @@
+import pandas as pd
+from datetime import datetime
 from pprint import pprint
 from feast import FeatureStore
 
@@ -18,3 +20,24 @@ feature_vector = store.get_online_features(
 
 pprint(feature_vector)
 
+entity_df = pd.DataFrame.from_dict(
+    {
+        "driver_id": [1001, 1002, 1003, 1004],
+        "event_timestamp": [
+            datetime(2021, 4, 12, 10, 59, 42),
+            datetime(2021, 4, 12, 8, 12, 10),
+            datetime(2021, 4, 12, 8, 12, 10),
+            datetime(2021, 4, 12, 8, 12, 10),
+        ],
+    }
+)
+retrieval_job = store.get_historical_features(
+    entity_df=entity_df,
+    features=[
+        "driver_hourly_stats:conv_rate",
+        "driver_hourly_stats:acc_rate",
+        "driver_hourly_stats:avg_daily_trips",
+    ],
+)
+
+pprint(retrieval_job.to_df())
