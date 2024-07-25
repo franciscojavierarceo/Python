5d1c1e49a19bb058543f9df531f2c395b62e2516
Francisco Javier Arceo <farceo@redhat.com>
2024-07-25 16:34:57 -0400

    updated simple pipeline
    Signed-off-by: Francisco Javier Arceo <farceo@redhat.com>

--- demos/kfp-demos/feast-stocks/simple_kfp_example.py
+++ demos/kfp-demos/feast-stocks/simple_kfp_example.py
@@ -5,29 +5,55 @@ import json
 
 local.init(runner=local.SubprocessRunner())
 
+
 @dsl.component
-def add(a: int, b: int, out_artifact: Output[Artifact]):
+def add(a: int, b: int, out_artifact: Output[Artifact]) -> None:
     import json
 
     result = json.dumps(a + b)
 
-    with open(out_artifact.path, 'w') as f:
+    with open(out_artifact.path, "w") as f:
         f.write(result)
 
-    out_artifact.metadata['operation'] = 'addition'
+    out_artifact.metadata["operation"] = "addition"
 
 
-task = add(a=1, b=2)
-task2 = add(a=1, b=3)
-# can read artifact contents
-with open(task.outputs['out_artifact'].path) as f:
-    contents = f.read()
+@dsl.component
+def verify_result(artifact: Artifact, expected_value: int) -> None:
+    import json
 
-with open(task2.outputs['out_artifact'].path) as f2:
-    contents2 = f2.read()
+    with open(artifact.path) as f:
+        contents = f.read()
 
-assert json.loads(contents) == 3
-assert task.outputs['out_artifact'].metadata['operation'] == 'addition'
+    result = json.loads(contents)
+    assert result == expected_value
+    assert artifact.metadata["operation"] == "addition"
 
-assert json.loads(contents2) == 4
-assert task2.outputs['out_artifact'].metadata['operation'] == 'addition'
+
+@dsl.pipeline(
+    name="Simple pipeline",
+    description="Some basic tests",
+)
+def add_pipeline() -> None:
+    task1 = add(a=1, b=2)
+    task2 = add(a=1, b=3)
+
+    verify_task1 = verify_result(
+        artifact=task1.outputs["out_artifact"], expected_value=3
+    )
+    verify_task2 = verify_result(
+        artifact=task2.outputs["out_artifact"], expected_value=4
+    )
+
+    verify_task1.after(task1)
+    verify_task2.after(task2)
+
+
+# Compile and run the pipeline
+if __name__ == "__main__":
+    from kfp.compiler import Compiler
+
+    Compiler().compile(add_pipeline, "add_pipeline.yaml")
+
+    # Execute the pipeline directly
+    add_pipeline()
