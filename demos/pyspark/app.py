3d0c7517f235e793e968d14c0c6092774793f565
Francisco Javier Arceo <farceo@redhat.com>
2024-10-14 13:58:03 -0400

    formatted with black
    Signed-off-by: Francisco Javier Arceo <farceo@redhat.com>

--- demos/pyspark/app.py
+++ demos/pyspark/app.py
@@ -3,16 +3,18 @@ from pyspark.sql.functions import col
 import pandas as pd
 import numpy as np
 
+
 def generate_fake_data(num_records):
     # Generate fake data using pandas and numpy
     data = {
-        'id': np.arange(1, num_records + 1),
-        'name': ['Name_' + str(i) for i in range(1, num_records + 1)],
-        'age': np.random.randint(18, 65, size=num_records),
-        'salary': np.random.randint(30000, 100000, size=num_records)
+        "id": np.arange(1, num_records + 1),
+        "name": ["Name_" + str(i) for i in range(1, num_records + 1)],
+        "age": np.random.randint(18, 65, size=num_records),
+        "salary": np.random.randint(30000, 100000, size=num_records),
     }
     df = pd.DataFrame(data)
     return df
+
 
 def main():
     # Initialize Spark Session
@@ -26,8 +28,8 @@ def main():
     df = spark.createDataFrame(pandas_df)
 
     # Perform some transformations
-    df_filtered = df.filter(col('age') > 30)
-    df_avg_salary = df_filtered.groupBy().avg('salary')
+    df_filtered = df.filter(col("age") > 30)
+    df_avg_salary = df_filtered.groupBy().avg("salary")
 
     # Show the results
     print("Filtered Data:")
@@ -38,6 +40,6 @@ def main():
     # Stop the Spark session
     spark.stop()
 
+
 if __name__ == "__main__":
     main()
-
