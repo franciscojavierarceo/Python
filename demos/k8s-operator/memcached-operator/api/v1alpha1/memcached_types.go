0818da71130968ab822f8ee4e907a100d875a375
Francisco Javier Arceo <farceo@redhat.com>
2024-11-13 10:12:24 -0500

    updated types to include comments
    Signed-off-by: Francisco Javier Arceo <farceo@redhat.com>

--- demos/k8s-operator/memcached-operator/api/v1alpha1/memcached_types.go
+++ demos/k8s-operator/memcached-operator/api/v1alpha1/memcached_types.go
@@ -25,16 +25,25 @@ import (
 
 // MemcachedSpec defines the desired state of Memcached.
 type MemcachedSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-
+	// Size defines the number of Memcached instances
+    	// The following markers will use OpenAPI v3 schema to validate the value
+    	// More info: https://book.kubebuilder.io/reference/markers/crd-validation.html
+    	// +kubebuilder:validation:Minimum=1
+    	// +kubebuilder:validation:Maximum=3
+    	// +kubebuilder:validation:ExclusiveMaximum=false
 	Size int32 `json:"size,omitempty"`
 }
 
 // MemcachedStatus defines the observed state of Memcached.
 type MemcachedStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	// Represents the observations of a Memcached's current state.
+    	// Memcached.status.conditions.type are: "Available", "Progressing", and "Degraded"
+    	// Memcached.status.conditions.status are one of True, False, Unknown.
+    	// Memcached.status.conditions.reason the value should be a CamelCase string and producers of specific
+    	// condition types may define expected values and meanings for this field, and whether the values
+    	// are considered a guaranteed API.
+    	// Memcached.status.conditions.Message is a human readable message indicating details about the transition.
+    	// For further information see: https://github.com/kubernetes/community/blob/master/contributors/devel/sig-architecture/api-conventions.md#typical-status-properties
 	Conditions []metav1.Condition `json:"conditions,omitempty" patchStrategy:"merge" patchMergeKey:"type" protobuf:"bytes,1,rep,name=conditions"`
 }
 
