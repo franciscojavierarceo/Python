5bd0773783aed72d7b98a0c517e726644ab02171
Francisco Javier Arceo <farceo@redhat.com>
2024-11-13 10:06:46 -0500

    updated types and status definitions
    Signed-off-by: Francisco Javier Arceo <farceo@redhat.com>

--- demos/k8s-operator/memcached-operator/api/v1alpha1/memcached_types.go
+++ demos/k8s-operator/memcached-operator/api/v1alpha1/memcached_types.go
@@ -28,14 +28,15 @@ type MemcachedSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
 
-	// Foo is an example field of Memcached. Edit memcached_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	Size int32 `json:"size,omitempty"`
 }
 
 // MemcachedStatus defines the observed state of Memcached.
 type MemcachedStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
+	Conditions []metav1.Condition `json:"conditions,omitempty" patchStrategy:"merge" patchMergeKey:"type" protobuf:"bytes,1,rep,name=conditions"`
+
 }
 
 // +kubebuilder:object:root=true
