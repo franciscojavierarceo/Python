d1239dfe122e55a1edc94f566cbea4bc225b11be
Francisco Javier Arceo <farceo@redhat.com>
2024-11-13 10:10:20 -0500

    uploading diff from make and make generate
    Signed-off-by: Francisco Javier Arceo <farceo@redhat.com>

--- demos/k8s-operator/memcached-operator/api/v1alpha1/memcached_types.go
+++ demos/k8s-operator/memcached-operator/api/v1alpha1/memcached_types.go
@@ -36,7 +36,6 @@ type MemcachedStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
 	Conditions []metav1.Condition `json:"conditions,omitempty" patchStrategy:"merge" patchMergeKey:"type" protobuf:"bytes,1,rep,name=conditions"`
-
 }
 
 // +kubebuilder:object:root=true
