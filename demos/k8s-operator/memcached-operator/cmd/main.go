970b39dbdbc3d886f38bee8aca57d07cd34117b6
Francisco Javier Arceo <farceo@redhat.com>
2024-11-13 10:05:35 -0500

    adding files from the create api command
    Signed-off-by: Francisco Javier Arceo <farceo@redhat.com>

--- demos/k8s-operator/memcached-operator/cmd/main.go
+++ demos/k8s-operator/memcached-operator/cmd/main.go
@@ -34,6 +34,9 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/metrics/filters"
 	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
+
+	cachev1alpha1 "example.com/m/api/v1alpha1"
+	"example.com/m/internal/controller"
 	// +kubebuilder:scaffold:imports
 )
 
@@ -45,6 +48,7 @@ var (
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
+	utilruntime.Must(cachev1alpha1.AddToScheme(scheme))
 	// +kubebuilder:scaffold:scheme
 }
 
@@ -138,6 +142,13 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err = (&controller.MemcachedReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Memcached")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
